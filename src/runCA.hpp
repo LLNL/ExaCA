e825aaf5cb290b736295c544f5f9236ce53386a1
Sam Reeve <reevest@ornl.gov>
2021-05-17 12:00:55 -0400

    Update license header
    Former-commit-id: b2569c76c48c40d2b874bbcaf31539d8629db177

--- src/runCA.hpp
+++ src/runCA.hpp
@@ -1,5 +1,6 @@
-// Copyright 2021 Lawrence Livermore National Security, LLC and other
-// ExaCA Project Developers. See the top-level LICENSE file for details.
+// Copyright 2021 Lawrence Livermore National Security, LLC and other ExaCA Project Developers.
+// See the top-level LICENSE file for details.
+//
 // SPDX-License-Identifier: MIT
 
 #ifndef EXACA_RUN_HPP
