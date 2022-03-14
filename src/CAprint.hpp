05ebc6642deafe566cb3d1644e9befd534fbf05b
MattRolchigo <65y@ornl.gov>
2022-03-14 18:08:51 -0400

    Added option to print final cell undercooling values to file

--- src/CAprint.hpp
+++ src/CAprint.hpp
@@ -34,9 +34,9 @@ void PrintExaCAData(int id, int layernumber, int np, int nx, int ny, int nz, int
                     ViewI_H GrainID, ViewI_H GrainOrientation, ViewI_H CritTimeStep, ViewF_H GrainUnitVector,
                     ViewI_H LayerID, ViewI_H CellType, ViewF_H UndercoolingChange, ViewF_H UndercoolingCurrent,
                     std::string BaseFileName, int DecompositionStrategy, int NGrainOrientations, bool *Melted,
-                    std::string PathToOutput, int PrintDebug, bool PrintMisorientation, bool PrintFullOutput,
-                    bool PrintTimeSeries, int IntermediateFileCounter, int ZBound_Low, int nzActive, double deltax,
-                    float XMin, float YMin, float ZMin);
+                    std::string PathToOutput, int PrintDebug, bool PrintMisorientation, bool PrintFinalUndercooling,
+                    bool PrintFullOutput, bool PrintTimeSeries, int IntermediateFileCounter, int ZBound_Low,
+                    int nzActive, double deltax, float XMin, float YMin, float ZMin);
 void PrintExaCALog(int id, int np, std::string InputFile, std::string SimulationType, int DecompositionStrategy,
                    int MyXSlices, int MyYSlices, int MyXOffset, int MyYOffset, double AConst, double BConst,
                    double CConst, double DConst, double FreezingRange, double deltax, double NMax, double dTN,
@@ -57,6 +57,9 @@ void PrintGrainMisorientations(std::string BaseFileName, std::string PathToOutpu
                                ViewI3D_H Melted_WholeDomain, ViewI3D_H GrainID_WholeDomain, ViewI_H GrainOrientation,
                                ViewF_H GrainUnitVector, int NGrainOrientations, double deltax, float XMin, float YMin,
                                float ZMin);
+void PrintFinalUndercooling(std::string BaseFileName, std::string PathToOutput, int nx, int ny, int nz,
+                            ViewI3D_H Melted_WholeDomain, ViewF3D_H UndercoolingCurrent_WholeDomain, double deltax,
+                            float XMin, float YMin, float ZMin);
 void PrintIntermediateExaCAState(int IntermediateFileCounter, int layernumber, std::string BaseFileName,
                                  std::string PathToOutput, int ZBound_Low, int nzActive, int nx, int ny,
                                  ViewI3D_H GrainID_WholeDomain, ViewI3D_H CellType_WholeDomain,
