bf4c6ac81266ac1dd7b9e896ca9be7c4aace8d5f
MattRolchigo <65y@ornl.gov>
2022-05-18 09:36:36 -0400

    Logic for cells melting/multiple solidification events per cell, added/updated unit tests

--- unit_test/tstKokkosUpdate.hpp
+++ unit_test/tstKokkosUpdate.hpp
@@ -1,5 +1,6 @@
 #include <Kokkos_Core.hpp>
 
+#include "CAinitialize.hpp"
 #include "CAtypes.hpp"
 #include "CAupdate.hpp"
 
@@ -136,9 +137,204 @@ void testNucleation() {
     }
 }
 
+void testFillSteeringVector_Remelt() {
+
+    int id, np;
+    // Get number of processes
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    // Get individual process ID
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+
+    // Create views - each rank has 125 cells, 75 of which are part of the active region of the domain
+    int MyXSlices = 5;
+    int MyYSlices = 5;
+    int MyYOffset = 5 * id;
+    int nz = 5;
+    int ZBound_Low = 2;
+    int nzActive = 3;
+    int LocalActiveDomainSize = MyXSlices * MyYSlices * nzActive;
+    int LocalDomainSize = MyXSlices * MyYSlices * nz;
+
+    // MPI rank locations relative to the global grid
+    int DecompositionStrategy = 1; // 1D decomposition in the Y direction
+    bool AtNorthBoundary = false;
+    bool AtSouthBoundary = false;
+    bool AtEastBoundary = false;
+    bool AtWestBoundary = false;
+
+    // Buffers for ghost node data (fixed size)
+    int BufSizeX = MyXSlices;
+    int BufSizeY = 0;
+    int BufSizeZ = nzActive;
+
+    // Send/recv buffers for ghost node data - initialize with values of 1.0
+    Buffer2D BufferSouthSend(Kokkos::ViewAllocateWithoutInitializing("BufferSouthSend"), BufSizeX * BufSizeZ, 5);
+    Buffer2D BufferNorthSend(Kokkos::ViewAllocateWithoutInitializing("BufferNorthSend"), BufSizeX * BufSizeZ, 5);
+    Buffer2D BufferEastSend(Kokkos::ViewAllocateWithoutInitializing("BufferEastSend"), BufSizeY * BufSizeZ, 5);
+    Buffer2D BufferWestSend(Kokkos::ViewAllocateWithoutInitializing("BufferWestSend"), BufSizeY * BufSizeZ, 5);
+    Buffer2D BufferNorthEastSend(Kokkos::ViewAllocateWithoutInitializing("BufferNorthEastSend"), BufSizeZ, 5);
+    Buffer2D BufferNorthWestSend(Kokkos::ViewAllocateWithoutInitializing("BufferNorthWestSend"), BufSizeZ, 5);
+    Buffer2D BufferSouthEastSend(Kokkos::ViewAllocateWithoutInitializing("BufferSouthEastSend"), BufSizeZ, 5);
+    Buffer2D BufferSouthWestSend(Kokkos::ViewAllocateWithoutInitializing("BufferSouthWestSend"), BufSizeZ, 5);
+    Kokkos::deep_copy(BufferSouthSend, 1.0);
+    Kokkos::deep_copy(BufferNorthSend, 1.0);
+
+    // Initialize neighbor lists
+    using memory_space = Kokkos::DefaultExecutionSpace::memory_space;
+    NList NeighborX, NeighborY, NeighborZ;
+    NeighborListInit(NeighborX, NeighborY, NeighborZ);
+
+    ViewI_H GrainID_Host(Kokkos::ViewAllocateWithoutInitializing("GrainID_Host"), LocalDomainSize);
+    ViewI_H CellType_Host(Kokkos::ViewAllocateWithoutInitializing("CellType_Host"), LocalDomainSize);
+    ViewI_H MeltTimeStep_Host(Kokkos::ViewAllocateWithoutInitializing("MeltTimeStep_Host"), LocalDomainSize);
+    ViewI_H CritTimeStep_Host(Kokkos::ViewAllocateWithoutInitializing("CritTimeStep_Host"), LocalDomainSize);
+    ViewF_H UndercoolingChange_Host(Kokkos::ViewAllocateWithoutInitializing("UndercoolingChange_Host"),
+                                    LocalDomainSize);
+    ViewF_H UndercoolingCurrent_Host("UndercoolingCurrent_Host",
+                                     LocalDomainSize); // initialize to 0, no initial undercooling
+
+    for (int i = 0; i < LocalDomainSize; i++) {
+        // Cell coordinates on this rank in X, Y, and Z (GlobalZ = relative to domain bottom)
+        int GlobalZ = i / (MyXSlices * MyYSlices);
+        int Rem = i % (MyXSlices * MyYSlices);
+        int RankY = Rem % MyYSlices;
+        // Let cells be assigned GrainIDs based on the rank ID
+        // Cells on rank 0 have grain ID 0, rank 1 have grain ID 1, etc
+        GrainID_Host(i) = id;
+        // Cells at Z = 0 through Z = 2 are Solid, Z = 3 and 4 are TempSolid
+        if (GlobalZ <= 2)
+            CellType_Host(i) = Solid;
+        else
+            CellType_Host(i) = TempSolid;
+        // Cells "melt" at a time step corresponding to their Y location in the overall domain (depends on MyYOffset of
+        // the rank)
+        MeltTimeStep_Host(i) = RankY + MyYOffset + 1;
+        // Cells reach liquidus during cooling 2 time steps after melting
+        CritTimeStep_Host(i) = MeltTimeStep_Host(i) + 2;
+        // Let the cooling rate of the cells from the liquidus depend on the rank ID
+        UndercoolingChange_Host(i) = 0.2 * id;
+    }
+
+    // Steering Vector
+    ViewI SteeringVector(Kokkos::ViewAllocateWithoutInitializing("SteeringVector"), LocalActiveDomainSize);
+    ViewI_H numSteer_Host(Kokkos::ViewAllocateWithoutInitializing("SteeringVectorSize"), 1);
+    numSteer_Host(0) = 0;
+
+    // Copy views to device for test
+    ViewI numSteer = Kokkos::create_mirror_view_and_copy(memory_space(), numSteer_Host);
+    ViewI GrainID = Kokkos::create_mirror_view_and_copy(memory_space(), GrainID_Host);
+    ViewI CellType = Kokkos::create_mirror_view_and_copy(memory_space(), CellType_Host);
+    ViewI MeltTimeStep = Kokkos::create_mirror_view_and_copy(memory_space(), MeltTimeStep_Host);
+    ViewI CritTimeStep = Kokkos::create_mirror_view_and_copy(memory_space(), CritTimeStep_Host);
+    ViewF UndercoolingChange = Kokkos::create_mirror_view_and_copy(memory_space(), UndercoolingChange_Host);
+    ViewF UndercoolingCurrent = Kokkos::create_mirror_view_and_copy(memory_space(), UndercoolingCurrent_Host);
+
+    int numcycles = 15;
+    for (int cycle = 1; cycle <= numcycles; cycle++) {
+        // Update cell types, local undercooling each time step, and fill the steering vector
+        FillSteeringVector_Remelt(cycle, LocalActiveDomainSize, MyXSlices, MyYSlices, NeighborX, NeighborY, NeighborZ,
+                                  CritTimeStep, UndercoolingCurrent, UndercoolingChange, CellType, GrainID, ZBound_Low,
+                                  nzActive, SteeringVector, numSteer, numSteer_Host, MeltTimeStep, BufSizeX, BufSizeY,
+                                  AtNorthBoundary, AtSouthBoundary, AtEastBoundary, AtWestBoundary, BufferWestSend,
+                                  BufferEastSend, BufferNorthSend, BufferSouthSend, BufferNorthEastSend,
+                                  BufferNorthWestSend, BufferSouthEastSend, BufferSouthWestSend, DecompositionStrategy);
+    }
+
+    // Copy CellType, SteeringVector, numSteer, UndercoolingCurrent, Buffers back to host to check steering vector
+    // construction results
+    CellType_Host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), CellType);
+    ViewI_H SteeringVector_Host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), SteeringVector);
+    numSteer_Host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), numSteer);
+    UndercoolingCurrent_Host = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), UndercoolingCurrent);
+    Buffer2D_H BufferSouthSend_Host = Kokkos::create_mirror_view_and_copy(memory_space(), BufferSouthSend);
+    Buffer2D_H BufferNorthSend_Host = Kokkos::create_mirror_view_and_copy(memory_space(), BufferNorthSend);
+
+    // Check the modified CellType and UndercoolingCurrent values on the host:
+    // Check that the cells corresponding to outside of the "active" portion of the domain have unchanged values
+    // Check that the cells corresponding to the "active" portion of the domain have potentially changed values
+    // Z = 3: Rank 0, with all cells having GrainID = 0, should have Liquid cells everywhere, with undercooling
+    // depending on the Y position Z = 3, Rank > 0, with GrainID > 0, should have TempSolid cells (if not enough time
+    // steps to melt), Liquid cells (if enough time steps to melt but not reach the liquidus again), or FutureActive
+    // cells (if below liquidus time step). If the cell is FutureActive type, it should also have a local undercooling
+    // based on the Rank ID and the time step that it reached the liquidus relative to numcycles Z = 4, all ranks:
+    // should have TempSolid cells (if not enough time steps to melt) or Liquid (if enough time steps to melt). Local
+    // undercooling should be based on the Rank ID and the time step that it reached the liquidus relative to numcycles
+    int FutureActiveCells = 0;
+    for (int i = 0; i < LocalDomainSize; i++) {
+        // Cell coordinates on this rank in X, Y, and Z (GlobalZ = relative to domain bottom)
+        int GlobalZ = i / (MyXSlices * MyYSlices);
+        if (GlobalZ <= 2) {
+            EXPECT_EQ(CellType_Host(i), Solid);
+            EXPECT_FLOAT_EQ(UndercoolingCurrent_Host(i), 0.0);
+        }
+        else {
+            if (numcycles < MeltTimeStep_Host(i)) {
+                EXPECT_EQ(CellType_Host(i), TempSolid);
+                EXPECT_FLOAT_EQ(UndercoolingCurrent_Host(i), 0.0);
+            }
+            else if ((numcycles >= MeltTimeStep_Host(i)) && (numcycles <= CritTimeStep_Host(i))) {
+                EXPECT_EQ(CellType_Host(i), Liquid);
+                EXPECT_FLOAT_EQ(UndercoolingCurrent_Host(i), 0.0);
+            }
+            else {
+                EXPECT_FLOAT_EQ(UndercoolingCurrent_Host(i), (numcycles - CritTimeStep_Host(i)) * 0.2 * id);
+                if ((id == 0) || (GlobalZ == 4))
+                    EXPECT_EQ(CellType_Host(i), Liquid);
+                else {
+                    EXPECT_EQ(CellType_Host(i), FutureActive);
+                    FutureActiveCells++;
+                }
+            }
+        }
+    }
+    std::cout << "Future active cells rank " << id << " : " << FutureActiveCells << std::endl;
+    // Check the steering vector values on the host
+    EXPECT_EQ(FutureActiveCells, numSteer_Host(0));
+    for (int i = 0; i < FutureActiveCells; i++) {
+        // This cell should correspond to a cell at GlobalZ = 3 (RankZ = 1), and some X and Y
+        int LowerBoundCellLocation = MyXSlices * MyYSlices - 1;
+        int UpperBoundCellLocation = 2 * MyXSlices * MyYSlices;
+        EXPECT_GT(SteeringVector_Host(i), LowerBoundCellLocation);
+        EXPECT_LT(SteeringVector_Host(i), UpperBoundCellLocation);
+    }
+
+    // Check that the buffer values were either appropriately set to zeros (if the cell underwent melting) or remained
+    // at 1.0
+    for (int i = 0; i < BufSizeX * BufSizeZ; i++) {
+        int RankZ = i / BufSizeX;
+        int GlobalZ = RankZ + ZBound_Low;
+        int RankX = i % BufSizeX;
+        int NorthCellCoordinate = GlobalZ * MyXSlices * MyYSlices + RankX * MyYSlices + (MyYSlices - 1);
+        if ((CellType_Host(NorthCellCoordinate) == TempSolid) || (CellType_Host(NorthCellCoordinate) == Solid)) {
+            for (int j = 0; j < 5; j++) {
+                EXPECT_EQ(BufferNorthSend_Host(i, j), 1.0);
+            }
+        }
+        else {
+            for (int j = 0; j < 5; j++) {
+                EXPECT_EQ(BufferNorthSend_Host(i, j), 0.0);
+            }
+        }
+        int SouthCellCoordinate = GlobalZ * MyXSlices * MyYSlices + RankX * MyYSlices + (MyYSlices - 1);
+        if ((CellType_Host(SouthCellCoordinate) == TempSolid) || (CellType_Host(SouthCellCoordinate) == Solid)) {
+            for (int j = 0; j < 5; j++) {
+                EXPECT_EQ(BufferNorthSend_Host(i, j), 1.0);
+            }
+        }
+        else {
+            for (int j = 0; j < 5; j++) {
+                EXPECT_EQ(BufferNorthSend_Host(i, j), 0.0);
+            }
+        }
+    }
+}
+
 //---------------------------------------------------------------------------//
 // RUN TESTS
 //---------------------------------------------------------------------------//
-TEST(TEST_CATEGORY, cell_update_tests) { testNucleation(); }
+TEST(TEST_CATEGORY, cell_update_tests) {
+    testNucleation();
+    testFillSteeringVector_Remelt();
+}
 
 } // end namespace Test
