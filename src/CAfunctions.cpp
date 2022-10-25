7c52c9946e421e01a8c46bdca035b5671eaf4ec5
Matt Rolchigo <40367841+MattRolchigo@users.noreply.github.com>
2022-10-25 10:38:16 -0400

    New function for calculating grain misorientation (#134)

--- src/CAfunctions.cpp
+++ src/CAfunctions.cpp
@@ -143,3 +143,24 @@ void InitialDecomposition(int id, int np, int &NeighborRank_North, int &Neighbor
     else
         AtSouthBoundary = false;
 }
+
+// Create a view of size "NumberOfOrientation" of the misorientation of each possible grain orientation with the X, Y,
+// or Z directions (dir = 0, 1, or 2, respectively)
+ViewF_H MisorientationCalc(int NumberOfOrientations, ViewF_H GrainUnitVector, int dir) {
+
+    ViewF_H GrainMisorientation(Kokkos::ViewAllocateWithoutInitializing("GrainMisorientation"), NumberOfOrientations);
+    // Find the smallest possible misorientation between the specified direction, and this grain orientations' 6
+    // possible 001 directions (where 62.7 degrees is the largest possible misorientation between two 001 directions
+    // for a cubic crystal system)
+    for (int n = 0; n < NumberOfOrientations; n++) {
+        float MisorientationAngleMin = 62.7;
+        for (int ll = 0; ll < 3; ll++) {
+            float Misorientation = std::abs((180 / M_PI) * acos(GrainUnitVector(9 * n + 3 * ll + dir)));
+            if (Misorientation < MisorientationAngleMin) {
+                MisorientationAngleMin = Misorientation;
+            }
+        }
+        GrainMisorientation(n) = MisorientationAngleMin;
+    }
+    return GrainMisorientation;
+}
