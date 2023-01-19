a7cdb79174b8d3edfb0784a5b84830cec4275a28
Sam Reeve <6740307+streeve@users.noreply.github.com>
2023-01-19 14:53:44 -0600

    Make grain analysis into library

--- analysis/src/runGA.cpp
+++ analysis/src/runGA.cpp
@@ -3,7 +3,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-#include "runGA.hpp"
 #include "GAprint.hpp"
 #include "GAutils.hpp"
 
--- analysis/src/runGA_AMB.cpp
+++ analysis/src/runGA_AMB.cpp
@@ -3,7 +3,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-#include "runGA_AMB.hpp"
 #include "CAinitialize.hpp"
 #include "CAparsefiles.hpp"
 #include "CAtypes.hpp"
