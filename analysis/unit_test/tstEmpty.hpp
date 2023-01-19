65b16cbe5989f34ec630187e63725f787ffc49b2
Sam Reeve <6740307+streeve@users.noreply.github.com>
2023-01-19 14:54:19 -0600

    Share unit tests across main and analysis

---
+++ analysis/unit_test/tstEmpty.hpp
@@ -0,0 +1,19 @@
+// Copyright 2021-2022 Lawrence Livermore National Security, LLC and other ExaCA Project Developers.
+// See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: MIT
+
+#include <Kokkos_Core.hpp>
+
+#include "GAutils.hpp"
+
+#include <gtest/gtest.h>
+
+namespace Test {
+//---------------------------------------------------------------------------//
+//---------------------------------------------------------------------------//
+// RUN TESTS
+//---------------------------------------------------------------------------//
+// THIS SHOULD BE REMOVED.
+
+} // end namespace Test
---
+++ unit_test/test_harness.cmake
@@ -0,0 +1,87 @@
+
+##--------------------------------------------------------------------------##
+## Create tests without Kokkos
+##--------------------------------------------------------------------------##
+macro(ExaCA_add_tests_nobackend)
+  cmake_parse_arguments(EXACA_UNIT_TEST "MPI" "PACKAGE" "NAMES" ${ARGN})
+  set(EXACA_UNIT_TEST_MPIEXEC_NUMPROCS 1)
+  if(EXACA_UNIT_TEST_MPI)
+    foreach( _np 2 4 )
+      if(MPIEXEC_MAX_NUMPROCS GREATER_EQUAL ${_np})
+        list(APPEND EXACA_UNIT_TEST_MPIEXEC_NUMPROCS ${_np})
+      endif()
+    endforeach()
+    if(MPIEXEC_MAX_NUMPROCS GREATER 4)
+      list(APPEND EXACA_UNIT_TEST_MPIEXEC_NUMPROCS ${MPIEXEC_MAX_NUMPROCS})
+    endif()
+  endif()
+  set(EXACA_UNIT_TEST_MAIN ${TEST_HARNESS_DIR}/unit_test_main_nobackend.cpp)
+
+  set(_dir ${CMAKE_CURRENT_SOURCE_DIR})
+  foreach(_test ${EXACA_UNIT_TEST_NAMES})
+    set(_target ExaCA_${_test}_test)
+    add_executable(${_target} tst${_test}.cpp ${EXACA_UNIT_TEST_MAIN})
+    target_link_libraries(${_target} ${EXACA_UNIT_TEST_PACKAGE} ${gtest_target})
+
+    foreach(_np ${EXACA_UNIT_TEST_MPIEXEC_NUMPROCS})
+      add_test(NAME ${_target}_np_${_np} COMMAND
+        ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${_np} ${MPIEXEC_PREFLAGS}
+        $<TARGET_FILE:${_target}> ${MPIEXEC_POSTFLAGS} ${gtest_args})
+    endforeach()
+  endforeach()
+endmacro()
+
+##--------------------------------------------------------------------------##
+## Create main tests
+##--------------------------------------------------------------------------##
+macro(ExaCA_add_tests)
+  cmake_parse_arguments(EXACA_UNIT_TEST "MPI" "PACKAGE" "NAMES" ${ARGN})
+  set(EXACA_UNIT_TEST_MPIEXEC_NUMPROCS 1)
+  if(EXACA_UNIT_TEST_MPI)
+    foreach( _np 2 4 )
+      if(MPIEXEC_MAX_NUMPROCS GREATER_EQUAL ${_np})
+        list(APPEND EXACA_UNIT_TEST_MPIEXEC_NUMPROCS ${_np})
+      endif()
+    endforeach()
+    if(MPIEXEC_MAX_NUMPROCS GREATER 4)
+      list(APPEND EXACA_UNIT_TEST_MPIEXEC_NUMPROCS ${MPIEXEC_MAX_NUMPROCS})
+    endif()
+  endif()
+  set(EXACA_UNIT_TEST_NUMTHREADS 1)
+  foreach( _nt 2 4 )
+    if(MPIEXEC_MAX_NUMPROCS GREATER_EQUAL ${_nt})
+      list(APPEND EXACA_UNIT_TEST_NUMTHREADS ${_nt})
+    endif()
+  endforeach()
+  set(EXACA_UNIT_TEST_MAIN ${TEST_HARNESS_DIR}/unit_test_main.cpp)
+  foreach(_device ${EXACA_TEST_DEVICES})
+    set(_dir ${CMAKE_CURRENT_BINARY_DIR}/${_device})
+    file(MAKE_DIRECTORY ${_dir})
+    foreach(_test ${EXACA_UNIT_TEST_NAMES})
+      set(_file ${_dir}/tst${_test}_${_device}.cpp)
+      file(WRITE ${_file}
+        "#include <Test${_device}_Category.hpp>\n"
+        "#include <tst${_test}.hpp>\n"
+      )
+      set(_target ExaCA_${_test}_test_${_device})
+      add_executable(${_target} ${_file} ${EXACA_UNIT_TEST_MAIN})
+      target_include_directories(${_target} PRIVATE ${_dir}
+        ${TEST_HARNESS_DIR} ${CMAKE_CURRENT_SOURCE_DIR})
+      target_link_libraries(${_target} ${EXACA_UNIT_TEST_PACKAGE} ${gtest_target})
+
+      foreach(_np ${EXACA_UNIT_TEST_MPIEXEC_NUMPROCS})
+        if(_device STREQUAL PTHREAD OR _device STREQUAL OPENMP)
+          foreach(_thread ${EXACA_UNIT_TEST_NUMTHREADS})
+            add_test(NAME ${_target}_np_${_np}_nt_${_thread} COMMAND
+              ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${_np} ${MPIEXEC_PREFLAGS}
+              $<TARGET_FILE:${_target}> ${MPIEXEC_POSTFLAGS} ${gtest_args} --kokkos-threads=${_thread})
+          endforeach()
+        else()
+	    add_test(NAME ${_target}_np_${_np} COMMAND
+              ${MPIEXEC_EXECUTABLE} ${MPIEXEC_NUMPROC_FLAG} ${_np} ${MPIEXEC_PREFLAGS}
+              $<TARGET_FILE:${_target}> ${MPIEXEC_POSTFLAGS} ${gtest_args})
+        endif()
+      endforeach()
+    endforeach()
+  endforeach()
+endmacro()
