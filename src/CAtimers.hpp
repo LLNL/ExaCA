f4de5fec7cf9d98a699c1e997f0552009fab10e8
Sam Reeve <6740307+streeve@users.noreply.github.com>
2024-02-14 08:19:18 -0500

    Create timers class

---
+++ src/CAtimers.hpp
@@ -0,0 +1,135 @@
+// Copyright 2021-2023 Lawrence Livermore National Security, LLC and other ExaCA Project Developers.
+// See the top-level LICENSE file for details.
+//
+// SPDX-License-Identifier: MIT
+
+#ifndef EXACA_TIMERS_HPP
+#define EXACA_TIMERS_HPP
+
+#include "mpi.h"
+
+// Print timing info
+struct Timers {
+
+    int id;
+    double init_time = 0.0, nucl_time = 0.0, create_sv_time = 0.0, capture_time = 0.0, ghost_time = 0.0;
+    double start_nucl_time, start_create_sv_time, start_capture_time, start_ghost_time;
+    double start_init_time, start_run_time, start_out_time;
+    double run_time, out_time;
+    double layer_time_1, layer_time_2;
+
+    double init_max_time, init_min_time, out_max_time, out_min_time = 0.0;
+    double nucl_max_time, nucl_min_time, create_sv_min_time, create_sv_max_time, capture_max_time, capture_min_time,
+        ghost_max_time, ghost_min_time = 0.0;
+
+    Timers(const int mpi_id)
+        : id(mpi_id) {}
+
+    void startInit() { start_init_time = MPI_Wtime(); }
+    void endInit() {
+        init_time = MPI_Wtime() - start_init_time;
+        if (id == 0)
+            std::cout << "Data initialized: Time spent: " << init_time << " s" << std::endl;
+    }
+
+    void startRun() { start_run_time = MPI_Wtime(); }
+
+    void endRun() { run_time = MPI_Wtime() - start_run_time; }
+
+    void startOutput() { start_out_time = MPI_Wtime(); }
+
+    void endOutput() { out_time = MPI_Wtime() - start_out_time; }
+
+    void startNucleation() { start_nucl_time = MPI_Wtime(); }
+    void endNucleation() { nucl_time += MPI_Wtime() - start_nucl_time; }
+
+    void startSV() { start_create_sv_time = MPI_Wtime(); }
+    void endSV() { create_sv_time += MPI_Wtime() - start_create_sv_time; }
+
+    void startCapture() { start_capture_time = MPI_Wtime(); }
+    void endCapture() { capture_time += MPI_Wtime() - start_capture_time; }
+
+    void startComm() { start_ghost_time = MPI_Wtime(); }
+    void endComm() { ghost_time += MPI_Wtime() - start_ghost_time; }
+
+    void startLayer() { layer_time_1 = MPI_Wtime(); }
+    void endLayer(const int layernumber) {
+        double layer_time_2 = MPI_Wtime();
+        if (id == 0)
+            std::cout << "Time for layer number " << layernumber << " was " << layer_time_2 - layer_time_1
+                      << " s, starting layer " << layernumber + 1 << std::endl;
+    }
+    void endLayer() {
+        double layer_time_2 = MPI_Wtime();
+        if (id == 0)
+            std::cout << "Time for final layer was " << layer_time_2 - layer_time_1 << " s" << std::endl;
+    }
+
+    double getTotal() { return init_time + run_time + out_time; }
+
+    auto printLog() {
+        // This assumes reduceMPI() has already been called.
+        std::stringstream log;
+        log << "   \"Timing\": {" << std::endl;
+        log << "       \"Runtime\": " << getTotal() << "," << std::endl;
+        log << "       \"InitRunOutputBreakdown\": [" << init_time << "," << run_time << "," << out_time << "],"
+            << std::endl;
+        log << "       \"MaxMininit_time\": [" << init_max_time << "," << init_min_time << "]," << std::endl;
+        log << "       \"MaxMinNucleationTime\": [" << nucl_max_time << "," << nucl_min_time << "]," << std::endl;
+        log << "       \"MaxMinSteeringVectorCreationTime\": [" << create_sv_max_time << "," << create_sv_min_time
+            << "]," << std::endl;
+        log << "       \"MaxMinCellCaptureTime\": [" << capture_max_time << "," << capture_min_time << "],"
+            << std::endl;
+        log << "       \"MaxMinGhostExchangeTime\": [" << ghost_max_time << "," << ghost_min_time << "]," << std::endl;
+        log << "       \"MaxMinOutputTime\": [" << out_max_time << "," << out_min_time << "]" << std::endl;
+        log << "   }" << std::endl;
+        return log.str();
+    }
+
+    void reduceMPI() {
+        // Reduce all times across MPI ranks
+        MPI_Allreduce(&init_time, &init_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce(&init_time, &init_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+        MPI_Allreduce(&nucl_time, &nucl_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce(&nucl_time, &nucl_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+        MPI_Allreduce(&create_sv_time, &create_sv_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce(&create_sv_time, &create_sv_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+        MPI_Allreduce(&capture_time, &capture_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce(&capture_time, &capture_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+        MPI_Allreduce(&ghost_time, &ghost_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce(&ghost_time, &ghost_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+        MPI_Allreduce(&out_time, &out_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+        MPI_Allreduce(&out_time, &out_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+    }
+
+    void printFinal(const int np, const int cycle) {
+
+        if (id != 0)
+            return;
+
+        std::cout << "===================================================================================" << std::endl;
+        std::cout << "Having run with = " << np << " processors" << std::endl;
+        std::cout << "Output written at cycle = " << cycle << std::endl;
+        std::cout << "Total time = " << init_time + run_time + out_time << std::endl;
+        std::cout << "Time spent initializing data = " << init_time << " s" << std::endl;
+        std::cout << "Time spent performing CA calculations = " << run_time << " s" << std::endl;
+        std::cout << "Time spent collecting and printing output data = " << out_time << " s\n" << std::endl;
+
+        std::cout << "Max/min rank time initializing data  = " << init_max_time << " / " << init_min_time << " s"
+                  << std::endl;
+        std::cout << "Max/min rank time in CA nucleation   = " << nucl_max_time << " / " << nucl_min_time << " s"
+                  << std::endl;
+        std::cout << "Max/min rank time in CA steering vector creation = " << create_sv_max_time << " / "
+                  << create_sv_min_time << " s" << std::endl;
+        std::cout << "Max/min rank time in CA cell capture = " << capture_max_time << " / " << capture_min_time << " s"
+                  << std::endl;
+        std::cout << "Max/min rank time in CA ghosting     = " << ghost_max_time << " / " << ghost_min_time << " s"
+                  << std::endl;
+        std::cout << "Max/min rank time exporting data     = " << out_max_time << " / " << out_min_time << " s\n"
+                  << std::endl;
+
+        std::cout << "===================================================================================" << std::endl;
+    }
+};
+
+#endif
--- src/runCA.cpp
+++ src/runCA.cpp
@@ -15,9 +15,9 @@ void runExaCA(int id, int np, std::string input_file) {
     // Run on the default space.
     using memory_space = Kokkos::DefaultExecutionSpace::memory_space;
 
-    double nucl_time = 0.0, create_sv_time = 0.0, capture_time = 0.0, ghost_time = 0.0;
-    double start_nucl_time, start_create_sv_time, start_capture_time, start_ghost_time;
-    double start_init_time = MPI_Wtime();
+    // Create timers
+    Timers timers(id);
+    timers.startInit();
 
     // Read input file
     Inputs inputs(id, input_file);
@@ -88,17 +88,16 @@ void runExaCA(int id, int np, std::string input_file) {
     // Initialize printing struct from inputs
     Print print(grid, np, inputs.print);
 
-    // If specified, print initial values in some views for debugging purposes
-    double init_time = MPI_Wtime() - start_init_time;
-    if (id == 0)
-        std::cout << "Data initialized: Time spent: " << init_time << " s" << std::endl;
+    // End of initialization
+    timers.endInit();
     MPI_Barrier(MPI_COMM_WORLD);
+
     int cycle = 0;
-    double start_run_time = MPI_Wtime();
+    timers.startRun();
     for (int layernumber = 0; layernumber < grid.number_of_layers; layernumber++) {
 
         int x_switch = 0;
-        double layer_time_1 = MPI_Wtime();
+        timers.startLayer();
 
         // Loop continues until all liquid cells claimed by solid grains
         do {
@@ -112,9 +111,9 @@ void runExaCA(int id, int np, std::string input_file) {
             // Update cells on GPU - undercooling and diagonal length updates, nucleation
             // Cells with a successful nucleation event are marked and added to a steering vector, later dealt with in
             // CellCapture
-            start_nucl_time = MPI_Wtime();
+            timers.startNucleation();
             nucleation.nucleateGrain(cycle, grid, celldata, interface);
-            nucl_time += MPI_Wtime() - start_nucl_time;
+            timers.endNucleation();
 
             // Update cells on GPU - new active cells, solidification of old active cells
             // Cell capture performed in two steps - first, adding cells of interest to a steering vector (different
@@ -123,26 +122,25 @@ void runExaCA(int id, int np, std::string input_file) {
             // Constrained/directional solidification problem cells only undergo 1 solidificaton event and are
             // initialized as liquid - the steering vector operation for this problem can be constructed using
             // FillSteeringVector_NoRemelt (a simplified version of FillSteeringVector_Remelt
-            start_create_sv_time = MPI_Wtime();
-
+            timers.startSV();
             if ((simulation_type == "C") || (simulation_type == "SingleGrain"))
                 fillSteeringVector_NoRemelt(cycle, grid, celldata, temperature, interface);
             else
                 fillSteeringVector_Remelt(cycle, grid, celldata, temperature, interface);
-            create_sv_time += MPI_Wtime() - start_create_sv_time;
+            timers.endSV();
 
-            start_capture_time = MPI_Wtime();
+            timers.startCapture();
             // Cell capture and checking of the MPI buffers to ensure that all appropriate interface updates in the halo
             // regions were recorded
             cellCapture(cycle, np, grid, irf, celldata, temperature, interface, orientation);
             checkBuffers(id, cycle, grid, celldata, interface, orientation.n_grain_orientations);
-            capture_time += MPI_Wtime() - start_capture_time;
+            timers.endCapture();
 
             if (np > 1) {
                 // Update ghost nodes
-                start_ghost_time = MPI_Wtime();
+                timers.startComm();
                 haloUpdate(cycle, id, grid, celldata, interface, orientation);
-                ghost_time += MPI_Wtime() - start_ghost_time;
+                timers.endComm();
             }
 
             if ((cycle % 1000 == 0) && (simulation_type != "SingleGrain")) {
@@ -198,56 +196,32 @@ void runExaCA(int id, int np, std::string input_file) {
 
             x_switch = 0;
             MPI_Barrier(MPI_COMM_WORLD);
-            double layer_time_2 = MPI_Wtime();
             cycle = 0;
-            if (id == 0)
-                std::cout << "Time for layer number " << layernumber << " was " << layer_time_2 - layer_time_1
-                          << " s, starting layer " << layernumber + 1 << std::endl;
+            timers.endLayer(layernumber);
         }
         else {
             MPI_Barrier(MPI_COMM_WORLD);
-            double layer_time_2 = MPI_Wtime();
-            if (id == 0)
-                std::cout << "Time for final layer was " << layer_time_2 - layer_time_1 << " s" << std::endl;
+            timers.endLayer();
         }
     }
-
-    double run_time = MPI_Wtime() - start_run_time;
-    double start_out_time = MPI_Wtime();
-
+    timers.endRun();
     MPI_Barrier(MPI_COMM_WORLD);
+    timers.startOutput();
+
     // Collect and print specified final fields to output files
     print.printInterlayer(id, np, grid.number_of_layers - 1, grid, celldata, temperature, interface, orientation);
 
     // Calculate volume fraction of solidified domain consisting of nucleated grains
     float vol_fraction_nucleated = celldata.calcVolFractionNucleated(id, grid);
+    // Get MPI timing statisticss
+    timers.endOutput();
+    timers.reduceMPI();
 
-    double out_time = MPI_Wtime() - start_out_time;
-    double init_max_time, init_min_time, out_max_time, out_min_time = 0.0;
-    double nucl_max_time, nucl_min_time, create_sv_min_time, create_sv_max_time, capture_max_time, capture_min_time,
-        ghost_max_time, ghost_min_time = 0.0;
-    MPI_Allreduce(&init_time, &init_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    MPI_Allreduce(&init_time, &init_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
-    MPI_Allreduce(&nucl_time, &nucl_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    MPI_Allreduce(&nucl_time, &nucl_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
-    MPI_Allreduce(&create_sv_time, &create_sv_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    MPI_Allreduce(&create_sv_time, &create_sv_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
-    MPI_Allreduce(&capture_time, &capture_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    MPI_Allreduce(&capture_time, &capture_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
-    MPI_Allreduce(&ghost_time, &ghost_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    MPI_Allreduce(&ghost_time, &ghost_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
-    MPI_Allreduce(&out_time, &out_max_time, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
-    MPI_Allreduce(&out_time, &out_min_time, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
-
-    // Print the log file with JSON format, timing information to the console
+    // Print the log file with JSON format
     inputs.printExaCALog(id, np, input_file, grid.ny_local, grid.y_offset, irf, grid.deltax, grid.number_of_layers,
-                         grid.layer_height, grid.nx, grid.ny, grid.nz, init_time, run_time, out_time, cycle,
-                         init_max_time, init_min_time, nucl_max_time, nucl_min_time, create_sv_min_time,
-                         create_sv_max_time, capture_max_time, capture_min_time, ghost_max_time, ghost_min_time,
-                         out_max_time, out_min_time, grid.x_min, grid.x_max, grid.y_min, grid.y_max, grid.z_min,
-                         grid.z_max, vol_fraction_nucleated);
-    if (id == 0)
-        printExaCATiming(np, init_time, run_time, out_time, cycle, init_max_time, init_min_time, nucl_max_time,
-                         nucl_min_time, create_sv_min_time, create_sv_max_time, capture_max_time, capture_min_time,
-                         ghost_max_time, ghost_min_time, out_max_time, out_min_time);
+                         grid.layer_height, grid.nx, grid.ny, grid.nz, timers, cycle, grid.x_min, grid.x_max,
+                         grid.y_min, grid.y_max, grid.z_min, grid.z_max, vol_fraction_nucleated);
+
+    // Print timing information to the console
+    timers.printFinal(np, cycle);
 }
