548aa5e670b955f77f84cda81921103ee9972e00
Matt Rolchigo <65y@ornl.gov>
2024-04-23 14:53:33 -0400

    Update copyright years (#330)
    * Update copyright for 2024
    
    * Removed year from copyright statement
    
    * Updated LICENSE file

--- src/CAtypes.hpp
+++ src/CAtypes.hpp
@@ -1,4 +1,4 @@
-// Copyright 2021-2023 Lawrence Livermore National Security, LLC and other ExaCA Project Developers.
+// Copyright Lawrence Livermore National Security, LLC and other ExaCA Project Developers.
 // See the top-level LICENSE file for details.
 //
 // SPDX-License-Identifier: MIT
