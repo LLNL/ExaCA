2e1921aa11b7aad3617fa6e12a9e2d9913319c4f
MattRolchigo <65y@ornl.gov>
2024-04-23 10:23:37 -0400

    Update copyright for 2024

--- src/CAorientation.hpp
+++ src/CAorientation.hpp
@@ -1,4 +1,4 @@
-// Copyright 2021-2023 Lawrence Livermore National Security, LLC and other ExaCA Project Developers.
+// Copyright 2021-2024 Lawrence Livermore National Security, LLC and other ExaCA Project Developers.
 // See the top-level LICENSE file for details.
 //
 // SPDX-License-Identifier: MIT
